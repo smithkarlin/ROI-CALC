ffce4e4ffc6dde54b3ea1a8c66183576f1674630
smithkarlin <smithkarlin24@gmail.com>
2026-01-11 21:38:41 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
-import numpy_financial as npf
 
-st.write("RFID ROI Calculator")
+st.title("Hello Streamlit")
+st.write("This is a simple test app.")
